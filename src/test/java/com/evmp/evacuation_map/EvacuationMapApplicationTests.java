b4ce17e15be45b5b6dcc8f067253100cab9a694b
Yun Ji <kimyunji9272@gmail.com>
2023-02-28 07:52:34 +0900

    Feat: Init code
    첫 프로젝트 코드

---
+++ src/test/java/com/evmp/evacuation_map/EvacuationMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evmp.evacuation_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvacuationMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
